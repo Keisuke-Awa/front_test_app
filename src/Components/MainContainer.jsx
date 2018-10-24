75904acdd659334dec59e530250d8f9a24880856
keisukeawazu <keisuke.awa.basket@gmail.com>
2018-10-24 17:14:38 +0900

    createdできた

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import update from 'react-addons-update';
 
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import FormContainer from './FormContainer';
 import ProductsContainer from './ProductsContainer';
 
 class MainContainer extends Component {
@@ -22,10 +27,23 @@ class MainContainer extends Component {
       });
   }
 
+  createProduct = (product) => {
+    console.log(product);
+    axios.post('http://localhost:3001/products', { product })
+      .then((response) => {
+        const newData = update(this.state.products, { $push: [response.data] });
+        this.setState({ products: newData });
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+  }
+
   render() {
     return (
       <div className="app-main">
-        <ProductsContainer productData={ this.state.products } />
+        <FormContainer createProduct={this.createProduct} />
+        <ProductsContainer productData={this.state.products} />
       </div>
     );
   }
