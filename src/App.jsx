0b66aa9aed72994b266eb8de7eb126ba6293068c
keisukeawazu <keisuke.awa.basket@gmail.com>
2018-10-24 16:35:25 +0900

    take index

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MainContainer from './Components/MainContainer';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <MainContainer />
+  </div>
+);
+
+export default App;
---
+++ src/Components/MainContainer.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import ProductsContainer from './ProductsContainer';
+
+class MainContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/products')
+      .then((results) => {
+        console.log(results);
+        this.setState({ products: results.data });
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+  }
+
+  render() {
+    return (
+      <div className="app-main">
+        <ProductsContainer productData={ this.state.products } />
+      </div>
+    );
+  }
+}
+
+export default MainContainer;
